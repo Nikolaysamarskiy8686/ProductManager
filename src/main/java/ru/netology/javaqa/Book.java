4e8c2fec0beb457a68c208a184cca002dbe0c286
Kolya <kolya.samarskiy.2015@mail.ru>
2023-06-05 23:06:48 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/Book.java
@@ -0,0 +1,14 @@
+package ru.netology.javaqa;
+public class Book extends Product {
+    private String author;
+
+    public Book(int id, String name, int price, String author) {   // конструктор с 4 значениями
+        super(id, name, price);
+        this.author = author;
+    }
+
+    public String getAuthor() {
+
+        return author;
+    }
+}
---
+++ src/main/java/ru/netology/javaqa/Repository.java
@@ -0,0 +1,45 @@
+package ru.netology.javaqa;
+
+public class Repository {
+    Product[] products = new Product[0];  //массив храняший все продукты размер 0
+
+    // возвращает все продукты (книги, смартфоны) массива
+    public Product[] findAll() {
+        return products;
+    }
+
+    // метод добавления продукта (книги, смартфоны) в репозиторий
+    public void add(Product product) {
+        Product[] tmp = new Product[products.length + 1]; // новый массив
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i]; // в i ячеку нового массива сохр. i ячейку Продактс
+        }
+        tmp[tmp.length - 1] = product; // последняя ячейка нового массива = перемен.Продактс
+        products = tmp; // перезаписываем переменную сохраняя ссылку на переменн.нового массива
+    }
+
+    //метод удаления продукта по идентификатору
+    public void removeById(int removeId) {
+        Product foundProduct = findById(removeId);
+        if (foundProduct == null) {
+            throw new NotFoundException(removeId);
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int index = 0;
+        for (Product product : products) {
+            if (product.getId() != removeId) {
+                tmp[index] = product;
+                index++;
+            }
+        }
+        products = tmp;
+    }
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
